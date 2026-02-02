ed46f89a4960ac3662f86ed1fcb26d39c27cd3f6
Haikal <haikalariadma07@gmail.com>
2026-02-03 07:40:28 +0800

    Fix mysqldump command for Linux Server

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -11,37 +11,28 @@ class BackupController extends Controller
 {
     public function download()
     {
-        // 1. Nama File
         $filename = "backup-smkn5-" . date('Y-m-d_H-i-s') . ".sql";
         $filePath = storage_path("app/" . $filename);
         
-        // 2. Konfigurasi Database
         $dbName = config('database.connections.mysql.database');
         $dbUser = config('database.connections.mysql.username');
         $dbPass = config('database.connections.mysql.password');
         $dbHost = config('database.connections.mysql.host');
 
-        // 3. Susun Perintah (Linux Style)
-        // Di Ubuntu, kita cukup panggil 'mysqldump' langsung.
-        // Kita tambahkan --column-statistics=0 untuk jaga-jaga jika Ubuntu pakai MySQL 8
-        // agar tidak error "Unknown table 'COLUMN_STATISTICS'".
-        
+        // PERINTAH BERSIH (Cocok untuk Server Linux/Ubuntu)
+        // Kita hapus --column-statistics=0 agar kompatibel dengan semua versi MySQL/MariaDB Server
         $passwordCmd = $dbPass ? "--password=\"$dbPass\"" : "";
         
-        $command = "mysqldump --user=\"$dbUser\" $passwordCmd --host=\"$dbHost\" --column-statistics=0 \"$dbName\" > \"$filePath\"";
+        $command = "mysqldump --user=\"$dbUser\" $passwordCmd --host=\"$dbHost\" \"$dbName\" > \"$filePath\"";
 
-        // 4. Eksekusi
-        // Timeout kita set 120 detik (2 menit) biar aman kalau data sudah besar
         $process = Process::fromShellCommandline($command);
         $process->setTimeout(120); 
         $process->run();
 
-        // 5. Cek Error
         if (!$process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
 
-        // 6. Download & Hapus File Temp
         return Response::download($filePath)->deleteFileAfterSend(true);
     }
 }
